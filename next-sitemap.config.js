93b6a19e81beb20333472bc4fa92f5161cb02b25
woolbo100 <woolbo100@gmail.com>
2026-04-02 23:08:35 +0900

    공식 도메인(busan-now.com) 주소 설정 반영

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://busan-now.pages.dev',
+  siteUrl: 'https://busan-now.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: './out',
--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "부산나우 (Busan Now)",
   description: "부산의 모든 것 - 축제, 혜택, 맛집, 핫플, 데이트 코스 정보를 한눈에!",
-  url: "https://busan-now.pages.dev",
+  url: "https://busan-now.com",
   ogImage: "/og-image.jpg",
   links: {
     github: "https://github.com/my-local-info",
